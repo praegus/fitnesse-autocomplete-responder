08358b592c0b10ca519a175133a3b5100083ccf2
Tom Heintzberger <tom.heintzberger@praegus.nl>
2017-02-17 15:18:38 +0100

    Initial commit

---
+++ src/nl/tcnh/fitnesse/responders/AutoCompleteResponder.java
@@ -0,0 +1,197 @@
+package nl.tcnh.fitnesse.responders;
+
+
+import fitnesse.FitNesseContext;
+import fitnesse.http.Request;
+import fitnesse.http.Response;
+import fitnesse.http.SimpleResponse;
+import fitnesse.responders.WikiPageResponder;
+import fitnesse.testrunner.WikiTestPage;
+import fitnesse.testsystems.ClassPath;
+import fitnesse.testsystems.slim.HtmlTableScanner;
+import fitnesse.testsystems.slim.Table;
+import fitnesse.testsystems.slim.TableScanner;
+import fitnesse.wiki.WikiPage;
+import nl.tcnh.fitnesse.responders.util.ClassFinder;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.*;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+
+/**
+ * Responder for use with autocomplete javascript.
+ * Adds testrunner classpaths to classloader, finds all classes that are imported and lists scenario's available
+ * Returns JSON containing all classes for the requested page with their public methods, including parameters (types) using reflection
+ * Also returns any scenario's on the testPage with their paramaters (names)
+ */
+
+public class AutoCompleteResponder extends WikiPageResponder {
+
+    private JSONObject json = new JSONObject();
+    private JSONObject jclasses = new JSONObject();
+    private JSONArray jscenarios = new JSONArray();
+    private List<String> packages = new ArrayList<>();
+
+
+    @Override
+    public Response makeResponse(FitNesseContext context, Request request) throws Exception {
+
+        WikiPage page = loadPage(context, request.getResource(), request.getMap());
+        setClassPathsForPage(page);
+        getAutoCompleteDataFromPage(context, page);
+
+        SimpleResponse response = new SimpleResponse();
+        response.setMaxAge(0);
+        response.setStatus(404);
+        response.setContentType("application/json");
+        response.setContent(json.toString(3));
+
+        return response;
+
+    }
+
+
+    private void getAutoCompleteDataFromPage(FitNesseContext context, WikiPage page) {
+        TableScanner scanner = new HtmlTableScanner(makeHtml(context, page));
+        for (int i = 0; i < scanner.getTableCount(); i++) {
+            Table t = scanner.getTable(i);
+            switch (t.getCellContents(0, 0).toLowerCase()) {
+                case "import":
+                    addPackage(t);
+                    break;
+                case "library":
+                    addLibraryFixture(t);
+                    break;
+                case "scenario":
+                    addScenario(t);
+                    break;
+                default:
+                    //Skip!
+                    break;
+            }
+        }
+
+        addClassesToAutocopleteList();
+        json.put("classes", jclasses);
+        json.put("scenarios", jscenarios);
+
+    }
+
+    private void addClassesToAutocopleteList() {
+        for (String pkg : packages) {
+            System.out.println("Get package info for: " + pkg);
+            List<Class> classes = new ArrayList<>();
+
+            try {
+                classes.addAll(ClassFinder.getClasses(pkg, false));
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
+
+            //output += pkg + "<BR/>";
+            for (Class klass : classes) {
+                JSONObject thisClass = new JSONObject();
+                thisClass.put("qualifiedName", klass.getName());
+                thisClass.put("readableName", splitCamelCase(klass.getSimpleName()));
+                thisClass.put("availableMethods", getMethods(klass));
+                jclasses.put(klass.getSimpleName(),thisClass);
+            }
+        }
+    }
+
+    private JSONArray getMethods (Class klass) {
+        JSONArray cMethods = new JSONArray();
+        try{
+            Method[] methods = klass.getMethods();
+            for (Method method : methods) {
+                JSONObject thisMethod = new JSONObject();
+                thisMethod.put("name", splitCamelCase(method.getName()));
+                if (method.getParameters().length > 0) {
+                    JSONArray params = new JSONArray();
+                    for (Parameter param : method.getParameters()) {
+                        params.put(param.getType().getSimpleName());
+                    }
+                    thisMethod.put("parameters", params);
+                }
+                cMethods.put(thisMethod);
+            }
+        } catch (NoClassDefFoundError err) {
+            System.err.println(err.getMessage());
+        }
+        return cMethods;
+    }
+
+    private void addLibraryFixture(Table t) {
+        //elke rij (behalve header) toevoegen aan classes
+        //TODO: Implement
+    }
+
+    private void addPackage(Table t) {
+        for (int row = 1; row < t.getRowCount(); row++) {
+            packages.add(t.getCellContents(0, row));
+        }
+    }
+
+    private void addScenario(Table t) {
+        String scenarioName = "";
+        String params = "";
+
+        //elke oneven cell toevoegen aan scenarioname, even cellen aan params
+
+        JSONObject thisScenario = new JSONObject();
+        JSONArray parameters = new JSONArray();
+
+        for (int col = 1; col < t.getColumnCountInRow(0); col++) {
+            if ((col % 2) != 0) {
+                scenarioName += t.getCellContents(col, 0) + " ";
+            } else {
+                parameters.put(t.getCellContents(col, 0));
+            }
+        }
+
+        thisScenario.put("name", scenarioName);
+        thisScenario.put("parameters", parameters);
+        jscenarios.put(thisScenario);
+    }
+
+    private void setClassPathsForPage(WikiPage page) {
+        WikiTestPage testPage = new WikiTestPage(page);
+        ClassPath classPath = testPage.getClassPath();
+        for (String path : classPath.getElements()) {
+            File classPathItem = new File(path);
+            addToClassPath(classPathItem);
+        }
+    }
+
+    private static void addToClassPath(File file) {
+        try {
+            System.out.println("Adding: " + file.getPath() + " to CP");
+            URL url = file.toURI().toURL();
+            URLClassLoader classLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+            Method method = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
+            method.setAccessible(true);
+            method.invoke(classLoader, url);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static String splitCamelCase(String s) {
+        return s.replaceAll(
+                String.format("%s|%s|%s",
+                        "(?<=[A-Z])(?=[A-Z][a-z])",
+                        "(?<=[^A-Z])(?=[A-Z])",
+                        "(?<=[A-Za-z])(?=[^A-Za-z])"
+                ),
+                " "
+        ).toLowerCase();
+    }
+
+}
---
+++ src/nl/tcnh/fitnesse/responders/util/ClassFinder.java
@@ -0,0 +1,117 @@
+package nl.tcnh.fitnesse.responders.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+/**
+ * Class to find classes by package, from fileSystem or JAR Based on code from https://dzone.com/articles/get-all-classes-within-package and
+ * http://stackoverflow.com/questions/11016092/how-to-load-classes-at-runtime-from-a-folder-or-jar *
+ */
+public class ClassFinder {
+
+    /**
+     * Scans all classes accessible from the context class loader which belong to the given package and subpackages.
+     *
+     * @param packageName The base package
+     * @return The classes
+     * @throws ClassNotFoundException
+     * @throws IOException
+     */
+
+    public static List<Class> getClasses(String packageName, boolean recursive)
+            throws ClassNotFoundException, IOException {
+        System.out.println("Getting classes for: " + packageName);
+
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        assert classLoader != null;
+        String path = packageName.replace('.', '/');
+        Enumeration resources = classLoader.getResources(path);
+        List<File> dirs = new ArrayList<>();
+
+        while (resources.hasMoreElements()) {
+            URL resource = (URL) resources.nextElement();
+            System.out.println("resource: " + resource.getPath());
+            dirs.add(new File(resource.getFile()));
+        }
+        List<Class> classes = new ArrayList<>();
+        for (File directory : dirs) {
+            System.out.println("adding classes for: " + directory.getPath());
+            classes.addAll(findClasses(directory, packageName, recursive));
+        }
+        return classes;
+    }
+
+    /**
+     * Recursive method used to find all classes in a given directory and subdirs.
+     *
+     * @param directory   The base directory
+     * @param packageName The package name for classes found inside the base directory
+     * @return The classes
+     * @throws ClassNotFoundException
+     */
+    public static List<Class> findClasses(File directory, String packageName, boolean recursive) throws ClassNotFoundException {
+        List<Class> classes = new ArrayList<>();
+        if (!directory.exists()) {
+            if (directory.getPath().contains(".jar")) {
+                System.out.println("Found a JAR: " + directory.getPath());
+                String jarFile = directory.getPath().split("!")[0].replace("file:\\", "");
+                classes.addAll(getClassesFromJar(jarFile, packageName));
+            }
+            return classes;
+        }
+
+        File[] files = directory.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                if (recursive) {
+                    assert !file.getName().contains(".");
+                    classes.addAll(findClasses(file, packageName + "." + file.getName(), recursive));
+                }
+            } else if (file.getName().endsWith(".class") && !file.getName().contains("$")) {
+                classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
+            }
+        }
+        return classes;
+    }
+
+    private static List<Class> getClassesFromJar(String jar, String pkg) {
+        List<Class> jarClasses = new ArrayList<>();
+        try {
+            JarFile jarFile = new JarFile(jar);
+            Enumeration<JarEntry> e = jarFile.entries();
+            URL[] urls = {new URL("jar:file:" + jar + "!/")};
+            URLClassLoader cl = URLClassLoader.newInstance(urls);
+
+            while (e.hasMoreElements()) {
+                JarEntry je = e.nextElement();
+                if (je.isDirectory() || !je.getName().endsWith(".class") || je.getName().contains("$")) {
+                    continue;
+                }
+                System.out.println("Class in jar: " + je.getName());
+                String className = je.getName().substring(0, je.getName().length() - 6);
+                className = className.replace('/', '.');
+                try {
+                    Class c = cl.loadClass(className);
+                    if(c.getName().contains(pkg)) {
+                        jarClasses.add(c);
+                    }
+                } catch (ClassNotFoundException ex) {
+                    System.err.println(ex.getMessage());
+                }
+            }
+
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+        return jarClasses;
+    }
+
+
+}
